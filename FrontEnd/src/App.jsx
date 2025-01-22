74b02ccc93d880d8441f916bf4b75297cfc220f4
Arnauud <arnaud.bourgin@gmail.com>
2025-01-22 15:50:57 +0100

    Update Installation project

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Error from "./pages/Error";
+import "./styles/main.scss";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App
